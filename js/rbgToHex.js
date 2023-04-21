ee9a37176ff3ff066e6a152962f3a02dd0c49cbc
carlos <sanzonettycar@gmail.com>
2023-04-21 09:38:35 -0400

    add comment to the code

--- js/rbgToHex.js
+++ js/rbgToHex.js
@@ -1,11 +1,22 @@
-function rgb(r, g, b) {
-  return [roundRgb(r), roundRgb(g), roundRgb(b)].map(el => {
-    return el < 16 ? "0" + el.toString(16) : el.toString(16);
-  }).join("").toUpperCase();
+/*
+Return 0 or 255 depending if the number is greates that 255
+or smallest 0
+@param {integer} c - the color of th param in rgb
+*/
+function roundNumber(c) {
+  return Math.min(Math.max(c, 0), 255)
 }
 
-function roundRgb(c) {
-  return Math.min(Math.max(c, 0), 255);
+/*
+return the hex value
+@param {integer} r - value of red color
+@param {integer} g - value of green color
+@param {integer} b - value of blue color
+*/
+function rgb(r, g, b) {
+  return [roundNumber(r), roundNumber(g), roundNumber(b)].map(el => {
+    return el < 16 ? "0" + el.toString(16) : el.toString(16);
+  }).join("").toUpperCase();
 }
 
 console.log(rgb(192, 14, 94));
