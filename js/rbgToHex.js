ec0434934b02b841e286d588ef3599cce596496e
carlos <sanzonettycar@gmail.com>
2023-04-21 09:21:30 -0400

    update solution

--- js/rbgToHex.js
+++ js/rbgToHex.js
@@ -1,44 +1,14 @@
 function rgb(r, g, b) {
-  const rgb = [r, g, b];
-  const hex = {
-    0: 0,
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-    10: "A",
-    11: "B",
-    12: "C",
-    13: "D",
-    14: "E",
-    15: "F"
-  };
-  const rgbToEx = rgb.map((el) => {
-    if (el > 255) {
-      el = 255;
-      console.log(el);
-      return el;
-    }
-
-    if (el < 0) {
-
-      console.log(el);
-      el = 0;
-    }
-    let hexIndex = Math.floor(el / 16)
-    return hexIndex;
-  })
-
-  let result = rgbToEx.map(el => {
-    return [hex[el], hex[el]]
-  })
-  return result.join().trim().split(",").join("");
+  return [roundRgb(r), roundRgb(g), roundRgb(b)].map(el => {
+    return el < 16 ? "0" + el.toString(16) : el.toString(16);
+  }).join("").toUpperCase();
 }
-rgb(0, 0, 0);
-console.log(rgb(300, 255, 255));
-console.log(rgb(255, 255, 255))
+
+function roundRgb(c) {
+  return Math.min(Math.max(c, 0), 255);
+}
+
+console.log(rgb(192, 14, 94));
+console.log(rgb(77, 206, 12));
+console.log(rgb(0, 0, -20));
+console.log(rgb(255, 255, 255));
