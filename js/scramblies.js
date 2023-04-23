4830e112adf467672494d6d6ec5c17217a43ac89
csc32 <sanzonettycar@gmail.com>
2023-04-23 11:44:49 -0400

    firts commit

--- js/scramblies.js
+++ js/scramblies.js
@@ -1,4 +1,5 @@
 function scramble(str1, str2) {
+<<<<<<< HEAD
 
   let words = []
   for (let i = 0; i < str2.length; i++) {
@@ -7,9 +8,21 @@ function scramble(str1, str2) {
     }
   }
   return words.join("") == str2
+=======
+  let word = "";
+  for (let i = 0; i <= str2; i++) {
+    if (str1.includes(str2[i])) {
+      word.concat(str2[i]);
+    }
+  }
+  return word == str2;
+>>>>>>> a5a1029 (a commit)
 }
 
 console.log(scramble("rkqodlw", "world"));
 console.log(scramble('cedewaraaossoqqyt', "codewars"));
+<<<<<<< HEAD
 console.log(scramble('katas', 'steak'))
 console.log(scramble('scriptjavx', 'javascript'))
+=======
+>>>>>>> a5a1029 (a commit)
